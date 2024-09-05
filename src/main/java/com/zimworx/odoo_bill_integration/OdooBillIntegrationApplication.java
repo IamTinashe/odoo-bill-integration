cd1b7b7000e2e620a51bd1c193343b919ea77bf4
IamTinashe <tinashetp@gmail.com>
2024-09-05 15:45:47 +0200

    project_setup

---
+++ src/main/java/com/zimworx/odoo_bill_integration/OdooBillIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.zimworx.odoo_bill_integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdooBillIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdooBillIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zimworx/odoo_bill_integration/OdooBillIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zimworx.odoo_bill_integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdooBillIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
